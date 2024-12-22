cc83ab2843ce2784ed61051c2342f74b543c5b54
Priyanka Sarkar <priyankasarkar070416@gmail.com>
2024-12-22 09:08:11 -0600

    Added a dummy /signup endpoint and resolved CORS issues

---
+++ backend/src/main/java/com/unfiled6748/password_generator_manager/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.unfiled6748.password_generator_manager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/unfiled6748/password_generator_manager/backend/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.unfiled6748.password_generator_manager.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults()) //enables cors
+                //The rest of your configurations
+                .build();
+    }
+}
